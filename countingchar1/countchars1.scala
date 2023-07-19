b798d0f92e025aa2e64b9857cf672319fb32433c
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-07-20 00:00:57 +0900

    modify countchars1.scala

---
+++ countingchar1/countchars1.scala
@@ -0,0 +1,16 @@
+//行の読み出し
+//scala .\countchars1.scala countchars1.scala
+
+import scala.io.Source
+
+object counting {
+       def main(args: Array[String]) = {
+              if (args.length > 0) {
+                     for (line <- Source.fromFile(args(0)).getLines())
+                            println(line.length.toString + " " + line)
+              }
+              else {
+                     Console.err.println("Please enter filename.")       
+              }
+       }
+}
