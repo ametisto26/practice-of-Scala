715e6cb31e5bf4e274ba37c7208eea3c366c825d
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-07-22 15:18:36 +0900

    write about operators

--- chap5.scala
+++ chap5.scala
@@ -74,3 +74,81 @@ println(f"No\\\\escape!") //No\\escape!
 
 val pi = "Pi"
 println(f"${pi} is approximately ${math.Pi}%.5f.")
+
+
+//演算子（算術演算子）
+val sum = 1 + 2
+val b = sum == 1.+(2)
+
+val sumLong = 1 + 2L
+val b = sum == 1.+(2L)
+
+val str = "Hello, World!"
+str indexOf "," // ',' でも動く。
+
+str indexOf 'o' //Scala は str.indexOf('o')を呼び出す。
+str indexOf ('o', 4)
+str indexOf ('o', 5)
+
+str.indexOf('W')
+
+import scala.language.postfixOps
+val strlower = str toLowerCase
+val strCapit1 = str capitalize
+val strCapit2 = strlower capitalize
+
+val a = -2.02
+val a2 = (2.02).unary_-
+a == a2
+
+1 + 2.3
+3 - 1
+'b' - 'a'
+//"c" - "b"
+5L - 4
+8L * 6l
+11 / 4
+11 / 4.0
+11 % 4
+11 % 4.0
+11 % 4f
+
+math.IEEEremainder(11, 4) //-1.0
+
+val neg = 1 + -3
+-neg
+
+//関係演算子
+1 > 1
+1 < 2
+3.5f < 3.6f
+5L <= 5d
+'a' > 'A'
+val utrue = !true
+
+//論理演算子
+def salt() = {println("salt"); false}
+def pepper() = {println("pepper"); true}
+
+pepper() && salt()
+salt() && pepper()
+
+pepper() & salt()
+salt() & pepper()
+
+pepper() || salt()
+salt() || pepper()
+
+pepper() | salt()
+salt() | pepper()
+
+
+//ビット単位演算子
+1 & 2
+1 | 2
+1 ^ 3
+~1 //単項演算子
+
+1 << 2
+-1 >> 31
+-1 >>> 31 //符号なし
