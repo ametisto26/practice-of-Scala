2f6dcc3abbd2d46f3d8cc7ebfd671c636e614077
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-07-24 02:59:34 +0900

    update chap6

--- chap6.scala
+++ chap6.scala
@@ -2,13 +2,28 @@
 
 class Rational(n: Int, d: Int) { 
        require(d != 0) //d != 0 を要請している。
-       val numer: Int = n
-       val denom: Int = d
+
+       private val g = gcd(n.abs, d.abs)
+       val numer = n / g
+       val denom = d / g
+
+       def this(n: Int) = this(n, 1) //補助コンストラクタ
+
        // println("Created " + n + "/" + d)
-       override def toString = s"$n/$d"
+       override def toString = s"$numer/$denom"
+
+       private def gcd(a: Int, b: Int): Int = 
+              if (b == 0) a else gcd(b, a % b)
+
        def add(that: Rational): Rational = {
               new Rational(numer * that.denom +that.numer * denom, denom * that.denom)
        }
+
+       def lessThan(that: Rational) =
+              this.numer * that.denom < that.numer * this.denom
+       
+       def max(that: Rational) = 
+              if (this.lessThan(that)) that else this
 }
 
 //import scala.language.postfixOps
@@ -20,3 +35,10 @@ val q3 = q1 add q2
 
 q1.numer
 q1.denom
+
+q1 lessThan q2 //true
+q1 max q2 // q2
+
+val z1 = new Rational(5)
+
+val q4 = new Rational(42, 66)
