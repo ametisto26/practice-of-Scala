40b130e4d3e5ec684efcb3cfada2d90c33583659
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-08-08 13:47:22 +0900

    update chap8

--- chap8.scala
+++ chap8.scala
@@ -22,3 +22,31 @@ object LongLines {
 // }
 
 
+import scala.io.Source
+
+object LongLines {
+       def processFile(filename: String, width: Int) = {
+              def processLine(line: String) = {
+                     if (line.length > width)
+                            println(filename + ": " + line.trim)
+              }
+              val source = Source.fromFile(filename)
+              for (line <- source.getLines())
+                     processLine(line)
+       }
+}
+
+
+// 関数リテラル
+(x: Int) => x + 1
+
+var increase = (x: Int) => x + 1
+increase(10)
+
+increase = (x: Int) => x + 2525 // increase が var ゆえに可能 
+increase(10)
+
+increase = (x: Int) => {
+       println("Hello!")
+       x + 9999
+}
