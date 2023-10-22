e5a2eb0382724a4abdfe83b28a8c42f6a41f112d
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-10-22 21:52:16 +0900

    test

---
+++ bbb.scala
@@ -0,0 +1,11 @@
+object Main {
+       import java.util.Scanner
+       val sc = new Scanner(System.in)
+       val a = sc.next
+
+       val ans = a + " san"
+
+       def main(args: Array[String]): Unit = {
+              println(ans)
+       }
+}
